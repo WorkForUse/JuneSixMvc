2ef0ec08d18cb5e6651cece5a2580bce13c903b8
hUser <149518339+WorkForUse@users.noreply.github.com>
2024-06-20 03:52:53 -0700

    Add files via upload

---
+++ LeapYearChecker/LeapYearChecker/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LeapYearChecker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int year = Convert.ToInt32(textBox1.Text);
+            if(year % 4 == 0)
+            {
+                MessageBox.Show("Year is a leap Year");
+            }
+            else
+            {
+                MessageBox.Show("Year is not a leap Year");
+            }
+        }
+    }
+}
